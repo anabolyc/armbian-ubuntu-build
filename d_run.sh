169eeca28b949901f0177e99907a6f17459137c2
andrey.malyshenko <andrey.malyshenko@gmail.com>
2018-11-14 20:10:42 +0100

    fixups

--- d_run.sh
+++ d_run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-docker run --rm --privileged -ti $(cat tag) /bin/bash
-#-v /media/storage-unprotected/armbian-build:/armbian-build 
+docker run --rm --privileged -v ./output:/armbian-build/build -ti $(cat tag) /bin/bash
